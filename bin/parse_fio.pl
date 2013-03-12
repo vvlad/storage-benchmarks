efe63a58840c67190089274aab1c9b09921f64e4
Bogdan Velcea <phil2k@gmail.com mailto:phil2k@gmail.com>
2013-03-12 16:25:10 +0200

    fixed typo in naming results for json output (parse_fio.pl)

--- bin/parse_fio.pl
+++ bin/parse_fio.pl
@@ -79,11 +79,9 @@ foreach $path (@ARGV) {
     }
   
   my $name=&basename($path);
-  if ($name=~/^([^.]+)\.(.+)$/) {
+  if ($name=~/^([^.]+)\..+$/) {
     my $cat=$1;
-    my $nam=$2;
     if ($nam ne "out") { # dont' extract category/description from fio files (.out) that doesn't have any other dots (.) !
-      $name=$nam;
       if (!length($category)) {
         $cat=~s/[-_]/ /g;
         $category=$cat;
